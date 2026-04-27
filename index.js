74a623d374aabc8da16f6ca8aa0cfd678cd5e2ec
Elakiya <elakiyasabarinath@gmail.com>
2026-04-27 15:14:46 +0530

    Nodejs Demo

---
+++ index.js
@@ -0,0 +1,14 @@
+// Simple npm project demo
+
+console.log("Hello from npm project 🚀");
+
+// Function example
+function greet(name) {
+    return `Hello, ${name}! Welcome to Node.js`;
+}
+
+console.log(greet("Elakiya"));
+
+// Simple math operation
+const add = (a, b) => a + b;
+console.log("Sum:", add(5, 3));
